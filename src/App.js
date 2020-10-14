e9a22b3d7b56707e33849547f2c7906f79589c7a
nosremetnarg <grant.alex.emerson@gmail.com>
2020-10-14 12:27:31 -0500

    contact rendering done and css for current tab selection

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Gallery from './components/Gallery';
 import ContactForm from './components/Contact';
 
 function App() {
+  const [contactSelected, setContactSelected] = useState(false);
+
   const [categories] = useState([
     {
       name: 'commercial',
@@ -22,15 +24,22 @@ function App() {
   return (
     <div>
       <Nav
-        categories={categories}
-        setCurrentCategory={setCurrentCategory}
-        currentCategory={currentCategory}
-      ></Nav>
+  categories={categories}
+  setCurrentCategory={setCurrentCategory}
+  currentCategory={currentCategory}
+  contactSelected={contactSelected}
+  setContactSelected={setContactSelected}
+></Nav>
       <main>
         <div>
-        <ContactForm></ContactForm>
-        <Gallery currentCategory={currentCategory}></Gallery>
-          <About></About>
+        {!contactSelected ? (
+  <>
+    <Gallery currentCategory={currentCategory}></Gallery>
+    <About></About>
+  </>
+) : (
+    <ContactForm></ContactForm>
+  )}
         </div>
       </main>
     </div>
