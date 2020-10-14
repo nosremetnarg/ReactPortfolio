f134f67bf570699777380ed681deec8598ebefb7
nosremetnarg <grant.alex.emerson@gmail.com>
2020-10-14 09:56:14 -0500

    form validation working

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,19 +1,42 @@
 import React, { useState } from 'react'
+import { validateEmail } from '../../utils/helpers';
+
 
 function ContactForm() {
 
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
     const { name, email, message } = formState;
+    const [errorMessage, setErrorMessage] = useState('');
+
 
     function handleChange(e) {
-        setFormState({...formState, [e.target.name]: e.target.value })
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            // isValid conditional statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+            } else {
+                setErrorMessage('');
+            }
+        } else {
+            if (!e.target.value.length) {
+                setErrorMessage(`${e.target.name} is required.`);
+            } else {
+                setErrorMessage('');
+            }
+            // console.log('errorMessage', errorMessage);
+        }
+        if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+          }
     }
 
     function handleSubmit(e) {
         e.preventDefault();
         console.log(formState);
-      }
-      
+    }
+
 
     // JSX
     return (
@@ -26,7 +49,7 @@ function ContactForm() {
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" defaultValue={email} name="email" onChange={handleChange}/>
+                    <input type="email" defaultValue={email} name="email" onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
